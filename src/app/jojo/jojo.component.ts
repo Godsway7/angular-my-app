ed924470171edbfa98bbe08df79d9e62924c69f6
Godsway7 <godsway.nyatuame@amalitech.org>
2020-07-07 16:06:21 +0100

    1 file modified, 4 added

---
+++ src/app/jojo/jojo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jojo',
+  templateUrl: './jojo.component.html',
+  styleUrls: ['./jojo.component.css']
+})
+export class JojoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
